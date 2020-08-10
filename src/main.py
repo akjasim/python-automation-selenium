1fa45c532c5298063ad2b67e9ee90237cf2e481d
akjasim <jasimjasim786@gmail.com>
2020-08-10 06:52:46 +0530

    login

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from config import USERNAME, PASSWORD
 
 browser = webdriver.Chrome(
     executable_path='/Users/akjasim/chromedriver/chromedriver')
@@ -7,7 +8,10 @@ browser = webdriver.Chrome(
 browser.get('https://www.instagram.com/')
 
 username_field = browser.find_element_by_name('username')
-username_field.send_keys('akjasim')
+username_field.send_keys(USERNAME)
 
-search_btn = browser.find_element_by_css_selector('input[type="submit"]')
-search_btn.click()
+username_field = browser.find_element_by_name('password')
+username_field.send_keys(PASSWORD)
+
+login_btn = browser.find_element_by_css_selector('button[type="submit"]')
+login_btn.click()
