86befc87fe25836ea9f71c6b157f2b1205d07b9a
akjasim <jasimjasim786@gmail.com>
2020-08-10 06:49:42 +0530

    updated gitignore

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
 from selenium import webdriver
+import time
 
-browser = webdriver.Chrome(executable_path='/')
+browser = webdriver.Chrome(
+    executable_path='/Users/akjasim/chromedriver/chromedriver')
+
+browser.get('https://www.instagram.com/')
+
+username_field = browser.find_element_by_name('username')
+username_field.send_keys('akjasim')
+
+search_btn = browser.find_element_by_css_selector('input[type="submit"]')
+search_btn.click()
