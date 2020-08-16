6e06cea4d30378b522a278382fe94aacc15dd0af
akjasim <jasimjasim786@gmail.com>
2020-08-16 09:47:59 +0530

    refactor

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from config import EMAIL, PASSWORD
 import pandas as pd
 
 dataframe = pd.read_excel('data.xlsx')
+# print(dataframe.index)
 
 browser = webdriver.Chrome(
     executable_path='/Users/akjasim/chromedriver/chromedriver')
@@ -12,28 +13,25 @@ browser.maximize_window()
 
 browser.get('https://www.gmail.com')
 
+email_field = browser.find_element_by_css_selector('input[type="email"]')
+email_field.send_keys(EMAIL)
 
-email_input = browser.find_element_by_css_selector('input[type="email"]')
-email_input.send_keys(EMAIL)
-
-next_btn = browser.find_element_by_class_name(
-    'VfPpkd-LgbsSe')
+next_btn = browser.find_element_by_class_name('VfPpkd-LgbsSe')
 next_btn.click()
 
 time.sleep(2)
 
-password_input = browser.find_element_by_name('password')
-password_input.send_keys(PASSWORD)
+password_field = browser.find_element_by_name('password')
+password_field.send_keys(PASSWORD)
 
-next_btn = browser.find_element_by_class_name(
-    'VfPpkd-LgbsSe')
-next_btn.click()
+login_btn = browser.find_element_by_class_name('VfPpkd-LgbsSe')
+login_btn.click()
 
-for index, i in enumerate(dataframe.index):
-    # print(dataframe.loc[i], end='\n\n')
-    # print(index)
+for i in dataframe.index:
+    print(dataframe.loc[i]['Name'])
+    # print(i)
 
-    time.sleep(3)
+    time.sleep(2)
 
     compose_btn = browser.find_element_by_css_selector('.T-I.T-I-KE.L3')
     compose_btn.click()
@@ -44,14 +42,15 @@ for index, i in enumerate(dataframe.index):
     to_field.send_keys(dataframe.loc[i]['Email'])
 
     subject_field = browser.find_element_by_name('subjectbox')
-    subject_field.send_keys(f"Hello {dataframe.loc[i]['Name']}")
+    subject_field.send_keys('Hiiii')
 
     body_field = browser.find_element_by_css_selector(
         '.Am.Al.editable.LW-avf.tS-tW')
-    body_content = f"""Hello {dataframe.loc[i]['Name']},
-Hiiiiaf,
-dasfsf
-fadsf
+    body_content = f"""Hii {dataframe.loc[i]['Name']},
+
+How fdsaf,
+asdfasd
+fasdf
     """
     body_field.send_keys(body_content)
 
